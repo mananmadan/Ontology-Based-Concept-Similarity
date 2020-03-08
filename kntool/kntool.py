0a2a218bbad9f55e0bfbb07b0847f34c283aa494
mananmadan <madan.manan1@gmail.com>
2020-03-08 14:18:24 +0530

    entity extraction is not working fine in the cases of long sentences

--- kntool/kntool.py
+++ kntool/kntool.py
@@ -1,14 +1,80 @@
+import nltk
 import spacy
 import re
 import pandas as pd
 import bs4
 import requests
 from spacy import displacy
-
+from nltk.tokenize import sent_tokenize
 nlp = spacy.load('en_core_web_sm')
-string = "The 22-year-old recently won ATP Challenger tournament."
+from spacy.matcher import Matcher
+from spacy.tokens import Span
+import networkx as nx
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+def get_entities(sent):
+  ## chunk 1
+  ent1 = ""
+  ent2 = ""
+
+  prv_tok_dep = ""    # dependency tag of previous token in the sentence
+  prv_tok_text = ""   # previous token in the sentence
+
+  prefix = ""
+  modifier = ""
+
+  #############################################################
+
+  for tok in nlp(sent):
+    ## chunk 2
+    # if token is a punctuation mark then move on to the next token
+    if tok.dep_ != "punct":
+      # check: token is a compound word or not
+      if tok.dep_ == "compound":
+        prefix = tok.text
+        # if the previous word was also a 'compound' then add the current word to it
+        if prv_tok_dep == "compound":
+          prefix = prv_tok_text + " "+ tok.text
+
+      # check: token is a modifier or not
+      if tok.dep_.endswith("mod") == True:
+        modifier = tok.text
+        # if the previous word was also a 'compound' then add the current word to it
+        if prv_tok_dep == "compound":
+          modifier = prv_tok_text + " "+ tok.text
+
+      ## chunk 3
+      if tok.dep_.find("subj") == True:
+        ent1 = modifier +" "+ prefix + " "+ tok.text
+        prefix = ""
+        modifier = ""
+        prv_tok_dep = ""
+        prv_tok_text = ""
+
+      ## chunk 4
+      if tok.dep_.find("obj") == True:
+        ent2 = modifier +" "+ prefix +" "+ tok.text
+
+      ## chunk 5
+      # update variables
+      prv_tok_dep = tok.dep_
+      prv_tok_text = tok.text
+  #############################################################
+
+  return [ent1.strip(), ent2.strip()]
+data_string = ""
+opentext = open("data.txt")
+readtext = opentext.read()
+sentences = sent_tokenize(readtext)
+count = 0
+for i in sentences:
+ print(count)
+ count= count+1
+ print(i)
+string = "Manan studies in college"
 unicode_string = unicode(string,"utf-8")
 doc = nlp(unicode_string)
-
+print("data")
+print(get_entities(unicode_string))
 for tok in doc:
   print(tok.text, "...", tok.dep_)
