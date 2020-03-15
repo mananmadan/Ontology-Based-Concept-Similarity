196a5d0def6d8a43cbd1300ad92f4b85b1dfee3d
mananmadan <madan.manan1@gmail.com>
2020-03-15 13:12:41 +0530

    developing kntool_v3 --> chunking extraction working fine

---
+++ kntool/kntool_v3.py
@@ -0,0 +1,55 @@
+import nltk
+import spacy
+import re
+import pandas as pd
+import bs4
+import requests
+from spacy import displacy
+from nltk.tokenize import sent_tokenize
+nlp = spacy.load('en_core_web_sm')
+from spacy.matcher import Matcher
+from spacy.tokens import Span
+import networkx as nx
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from nltk.tokenize import PunktSentenceTokenizer
+pst = PunktSentenceTokenizer()
+openfile = open("data.txt")
+data = openfile.read()
+tokenized_sentence = pst.tokenize(data)
+stringlist = []
+for i in tokenized_sentence:
+  try:
+    words = nltk.word_tokenize(i)
+    tagged = nltk.pos_tag(words)
+    chunkGram = r"""Chunk: {<JJ.?>*<NN.?>}"""
+    chunkParser = nltk.RegexpParser(chunkGram)
+    chunked = chunkParser.parse(tagged)
+    print(chunked)
+    stringlist.append(chunked.pformat().encode('ascii','ignore'))
+  except Exception as e:
+      print(str(e))
+#print(len(stringlist[1]))
+#String = stringlist[1]
+index = 0
+listoflist = []
+for f in stringlist:
+ String = f
+ #print(len(String))
+ chunklist = []
+ iter = re.finditer(r"\Chunk\b", String)
+ indices = [m.start(0) for m in iter]
+ #print(indices)
+ for x in indices:
+#print(stringlist[1][x+5])#space
+#get the word from space till /
+#print(x)
+  j=1
+  temp =""
+  while(stringlist[index][x+5+j]!='/'):
+   temp = temp + stringlist[index][x+5+j]
+   j = j+1
+ # print(temp)
+  chunklist.append(temp)
+ index = index + 1
+ print(chunklist)
