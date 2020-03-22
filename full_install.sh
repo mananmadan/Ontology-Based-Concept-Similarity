f44e3b622a7cb52ee7c7a5cf0c25583828e6c35d
mananmadan <madan.manan1@gmail.com>
2020-03-22 20:40:11 +0530

    auto update

--- full_install.sh
+++ full_install.sh
@@ -1,3 +1,31 @@
+p=7; 
+  
+for((m=1; m<=p; m++)) 
+do
+    # This loop print spaces 
+    # required 
+    for((a=i; a<=p; a++)) 
+    do
+      echo -ne " "; 
+    done
+      
+    # This loop print the left 
+    # side of the pyramid 
+    for((n=1; n<=m; n++)) 
+    do
+      echo -ne "#"; 
+    done
+  
+    # This loop print right  
+    # side of the pryamid. 
+    for((i=1; i<m; i++)) 
+    do
+      echo -ne "#"; 
+    done
+  
+    # New line 
+    echo; 
+done
 pip install nltk
 pip install pandas
 pip install matplotlib
