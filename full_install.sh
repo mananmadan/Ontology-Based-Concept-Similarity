42576ce0d36b641ae41b1b4e2e5463a6ea44f9cd
mananmadan <madan.manan1@gmail.com>
2020-03-22 23:30:18 +0530

    auto update

--- full_install.sh
+++ full_install.sh
@@ -1,31 +1,61 @@
-p=7; 
-  
-for((m=1; m<=p; m++)) 
+#little magic
+p=7;
+
+for((m=1; m<=p; m++))
 do
-    # This loop print spaces 
-    # required 
-    for((a=i; a<=p; a++)) 
+    for((n=1; n<=2; n++))
     do
-      echo -ne " "; 
+      echo -ne "#";
     done
-      
-    # This loop print the left 
-    # side of the pyramid 
-    for((n=1; n<=m; n++)) 
+ if ((m<=4))
+  then
+    for((a=1;a<=m; a++))
     do
-      echo -ne "#"; 
+      echo -ne " ";
     done
-  
-    # This loop print right  
-    # side of the pryamid. 
-    for((i=1; i<m; i++)) 
+fi
+if ((m<=4))
+ then
+    for((i=1; i<=2; i++))
     do
-      echo -ne "#"; 
+      echo -ne "#";
     done
-  
-    # New line 
-    echo; 
+  fi
+       for((j=0;j<=8-2*m; j=j+1))
+       do
+         echo -ne " ";
+       done
+
+   if ((m<=4))
+    then
+   for((i=1; i<=2; i++))
+   do
+     echo -ne "#";
+   done
+fi
+   if ((m<=4))
+    then
+      for((a=1;a<=m; a++))
+      do
+        echo -ne " ";
+      done
+  fi
+  if ((m>4))
+   then
+     for((a=0;a<=12; a++))
+     do
+       echo -ne " ";
+     done
+ fi
+
+  for((i=1; i<=2; i++))
+  do
+    echo -ne "#";
+  done
+
+    echo;
 done
+
 pip install nltk
 pip install pandas
 pip install matplotlib
