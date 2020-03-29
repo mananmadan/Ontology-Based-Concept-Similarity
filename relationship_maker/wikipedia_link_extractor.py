969a414d550dab0dbd33b3b5b8af72abd368be11
mananmadan <madan.manan1@gmail.com>
2020-03-29 12:56:48 +0530

    auto update

--- relationship_maker/wikipedia_link_extractor.py
+++ relationship_maker/wikipedia_link_extractor.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup as bs
 import requests
 import wikipedia
 
-search_string = "Data"
+search_string = "principles"
 #print(type(wikipedia.search(search_string)))
 print("Searching for .........")
 query = wikipedia.search(search_string)[0]
@@ -23,16 +23,15 @@ for link in soup.find_all("a"):
     url = link.get("href", "")
     if "//www.wikidata.org/" in url:
         wikidata.append(url)
-
+        print(url)
 #print(type(wikidata[0]))
 
 count = 0
 wikidata_id  = ""
 for i in wikidata[0]:
- if i == '/':
-  count = count + 1
- if count==5 and i != '/':
+ if i == 'Q' or i == '1' or i == '2' or i =='0' or i =='3' or i == '4' or i == '5' or i == '6' or i == '7' or i == '8' or i == '9':
   wikidata_id = wikidata_id + i
+  
 
 print(wikidata_id)
 
