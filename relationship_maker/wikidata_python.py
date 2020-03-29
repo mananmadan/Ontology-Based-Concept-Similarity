2032a588913892c13f7e444b6a5a81aae674cc43
mananmadan <madan.manan1@gmail.com>
2020-03-29 20:50:50 +0530

    auto update

---
+++ relationship_maker/wikidata_python.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 29 20:41:30 2020
+
+@author: manan
+"""
+from SPARQLWrapper import SPARQLWrapper,JSON
+import pandas as pd
+
+sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+sparql.setQuery("""
+SELECT ?item ?itemLabel
+WHERE
+{
+    ?item wdt:P361 wd:Q245652 .
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
+}
+""")
+
+sparql.setReturnFormat(JSON)
+results = sparql.query().convert()
+
+results_df = pd.io.json.json_normalize(results['results']['bindings'])
+#print(results_df)
+print(results_df[['item.value', 'itemLabel.value']].head())
